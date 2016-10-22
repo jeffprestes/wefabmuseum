d697c9ac086a76063bc3a6db9348673c620a2568
Jeff Prestes <jeffprestes@gmail.com>
2016-10-22 11:11:54 -0200

    First bot version

---
+++ index.js
@@ -0,0 +1,20 @@
+express = require('express');
+bodyParser = require('body-parser');
+request = require('request');
+app = express();
+
+app.use (bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT ||));
+
+app.get("/", function (req, res)  {
+  res.send("Este eh um Bot");
+});
+
+app.get("/webhook", function(req, res)  {
+  if (req.query['hub.verify_token']==='webfab_museum_token')  {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Token de validacao invalido');
+  }
+});
