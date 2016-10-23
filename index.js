3b9d817daa98a1409b11679bad8aaaf0d7218916
Jeff Prestes <jeffprestes@gmail.com>
2016-10-23 18:48:57 -0200

    Obtendo dados do usuario

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function receivedMessage (event)  {
             } else if (!erro && response.statusCode == 200) {
               console.log("[RECEIVED_MESSAGE]: Usuario retornado %s", JSON.stringify(body));
               var usuario = body;
-              console.log("[RECEIVED_MESSAGE]: Nome=" + usuario.first_name);
+              console.log("[RECEIVED_MESSAGE]: Nome=" + body.first_name);
 
               if (messageText == 'iniciar')  {
 
