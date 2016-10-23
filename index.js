94762cd4c31f2a824a56748a5cc3daf900e51e45
Jeff Prestes <jeffprestes@gmail.com>
2016-10-23 15:03:09 -0200

    Adicionando handlers para outros tipos de mensagem...

--- index.js
+++ index.js
@@ -2,32 +2,126 @@ express = require('express');
 bodyParser = require('body-parser');
 request = require('request');
 
-function sendMessage(recipientID, message)  {
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Funcoes de recebimento de mensagem
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function receivedAuthentication(event)  {
+  console.log("receivedAuthentication: ", event);
+}
+
+function receivedMessage (event)  {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Mensagem recebida para o usuario %d e a pagina %d em %d com mensagem: ",
+              senderID,
+              recipientID,
+              timeOfMessage,
+              console.log(JSON.stringify(message))
+              );
+
+  var messageID = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText)  {
+    switch (messageText) {
+      case 'image':
+        sendImageMessage(senderID);
+        break;
+
+      case 'button':
+        sendButtonMessage(senderID);
+        break;
+
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      case 'receipt':
+        sendReceiptMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Mensagem com anexo recebida");
+  }
+}
+
+function receivedDeliveryConfirmation(event)  {
+  console.log("Messagem entregue: ", event);
+}
+
+function receivedPostback(event)  {
+  console.log("receivedPostback: ", event);
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Funcoes de envio de mensagem
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function sendImageMessage(recipientID)  {
+  sendTextMessage(recipientID, "Respondendo uma imagem");
+}
+
+function sendButtonMessage(recipientID)  {
+  sendTextMessage(recipientID, "Respondendo um botao");
+}
+
+function sendGenericMessage(recipientID)  {
+  sendTextMessage(recipientID, "Respondendo um botao");
+}
+
+function sendReceiptMessage(recipientID)  {
+  sendTextMessage(recipientID, "Respondendo um recibo");
+}
+
+function sendTextMessage(recipientID, messageText)  {
+  var dado = {
+    recipient: {
+      id: recipientID
+    },
+    message:  {
+      text: messageText
+    }
+  };
+  console.log ("Enviado a mensagem: %d para %d", recipientID, dado);
+  envioGenerico(recipientID, dado);
+}
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Funcao de envio de mensagem com conteudo generico
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function envioGenerico(recipientID, messageData)  {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
     method: 'POST',
-    json: {
-      recipient: {id: recipientID},
-      message: message,
-    }
+    json: messageData
   }, function (erro, response, body) {
     if (erro)  {
       console.log("Erro enviando mensagem: ", erro);
     } else if (response.body.error) {
       console.log("erro: ", response.body.error);
     }
-  })
+  });
+
+  console.log ("Enviado a mensagem: %d para %d", recipientID, JSON.stringify(dado));
 }
 
+console.log ("Inicializando App...");
 
 app = express();
 
 app.use (bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 8181));
 
-console.log ("Inicializando App...");
+//Rotas
 app.get("/", function (req, res)  {
   res.send("Este eh um Bot e agora eh:" + Date.now());
 });
@@ -45,15 +139,41 @@ app.get("/webhook", function(req, res)  {
 
 app.post("/webhook", function(req, res) {
   console.log("Recieving Messages");
-  var events = req.body.entry[0].messaging;
-  for (i=0; i<events.length; i++) {
-    var event = events[i];
-    if (event.message && event.message.text)  {
-      sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-    }
+  var data = req.body;
+  console.log("Objeto recebido: ", data);
+
+  if (data.object == 'page')  {
+    data.entry.forEach (function (pageEntry)  {
+      var pageId = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      pageEntry.messaging.forEach (function (messagingEvent)  {
+
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+
+        } else if (messagingEvent.message)  {
+          receivedMessage(messagingEvent);
+
+        } else if (messagingEvent.delivery) {
+          recievedDeliveryConfirmation(messagingEvent);
+
+        } else if (messagingEvent.postback) {
+          recievedPostback(messagingEvent);
+
+        } else {
+          console.log("Webhook recebeu um evento de mensagem desconhecido: ", messagingEvent);
+        }
+
+      });
+
+    });
+
+    res.sendStatus(200);
   }
-  res.sendStatus(200);
 });
+
+app.listen((process.env.PORT || 8181));
 
 console.log ("Carregado App...");
 console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
