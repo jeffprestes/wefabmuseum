86ce1cc5c0136c54b66438e530f08b8cc3d39833
Jeff Prestes <jeffprestes@gmail.com>
2016-10-29 10:51:55 -0200

    Inclusao da funcao para remocao de acentos

--- index.js
+++ index.js
@@ -248,6 +248,20 @@ function obtemDadoUsuario(userId)   {
   });
 }
 
+/**
+ * Remove acentos de strings
+ * @param  {String} string acentuada
+ * @return {String} string sem acento
+ */
+
+var map={"ñ":"n","Ñ":"N","â":"a","Â":"A","à":"a","À":"A","á":"a","Á":"A","ã":"a","Ã":"A","ê":"e","Ê":"E","è":"e","È":"E","é":"e","É":"E","î":"i","Î":"I","ì":"i","Ì":"I","í":"i","Í":"I","õ":"o","Õ":"O","ô":"o","Ô":"O","ò":"o","Ò":"O","ó":"o","Ó":"O","ü":"u","Ü":"U","û":"u","Û":"U","ú":"u","Ú":"U","ù":"u","Ù":"U","ç":"c","Ç":"C"};
+
+function removerAcentos(s)  { 
+  return s.replace(/[\W\[\] ]/g, function(a) {
+    return map[a]||a;
+  }) 
+};
+
 console.log ("Inicializando App...");
 
 app = express();
